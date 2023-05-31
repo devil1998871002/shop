57fa45b5188805e5cad4bd3994ddc32798331d62
tsao <devil1998871002@gmail.com>
2023-05-31 13:49:03 +0800

    update shop

---
+++ all.js/member.js
@@ -0,0 +1,89 @@
+var vm = new Vue({
+    el:'.memberALL',
+    data:{
+        logo:'breakfast!',
+        menu:[{name:'首頁',link:'index.html'},{name:'產品列表',link:'product.html'},{name:'會員中心',link:'member.html'},{name:'購物車',link:'shopcar.html'}],
+        account:'',
+        password:'',
+        Newaccount:'',
+        Newpassword:'',
+        email:'devil1998871002@gmail.com',
+        socialLink:{
+            fb:'https://facebook.com/',
+            ig:'https://www.instagram.com/yuming7749/',
+            github:'https://github.com/devil1998871002'
+        },
+        log:{
+            type:true,
+            class:'log-content',
+            title:'會員登入',
+            color:'black',
+            Noaccount :false,
+            Nopassword:false,
+        },
+        reg:{
+            type:false,
+            class:'reg-content',
+            title:'會員註冊',
+            color:'gray',
+            Noaccount :false,
+            Nopassword:false,
+        },
+        
+
+    },
+    methods:{
+        golog(){
+            
+            this.log.color='black';
+            this.reg.color='gray';
+            this.log.type=true;
+            this.reg.type=false;
+        },
+        goreg(){
+            this.log.color='gray';
+            this.reg.color='black';
+            this.log.type=false;
+            this.reg.type=true;
+        },
+        logcheck(e){
+            if(this.account ==''&& this.password==''){
+                e.preventDefault();
+                this.log.Noaccount =true;
+                this.log.Nopassword=true;
+            }else if(this.account ==''){
+                e.preventDefault();
+                this.log.Noaccount =true;
+                this.log.Nopassword=false;
+            }else if(this.password==''){
+                e.preventDefault();
+                this.log.Noaccount =false;
+                this.log.Nopassword=true;
+            }else{
+                this.log.Noaccount =false;
+                this.log.Nopassword=false;
+                alert("目前不開放登入哦: )")
+            }
+        },
+        regcheck(e){
+            if(this.Newaccount ==''&& this.Newpassword==''){
+                e.preventDefault();
+                this.reg.Noaccount =true;
+                this.reg.Nopassword=true;
+            }else if(this.Newaccount ==''){
+                e.preventDefault();
+                this.reg.Noaccount =true;
+                this.reg.Nopassword=false;
+            }else if(this.Newpassword==''){
+                e.preventDefault();
+                this.reg.Noaccount =false;
+                this.reg.Nopassword=true;
+            }else{
+                this.reg.Noaccount =false;
+                this.reg.Nopassword=false;
+                alert("目前不開放註冊哦: )")
+            }
+        }
+    },
+
+})
---
+++ all.js/shopcar.js
@@ -0,0 +1,30 @@
+let list =JSON.parse(localStorage.getItem("mycoffee")) || [];
+var vm = new Vue({
+    el:'#shopcar',
+    data:{
+        logo:'breakfast!',
+        menu:[{name:'首頁',link:'index.html'},{name:'產品列表',link:'product.html'},{name:'會員中心',link:'member.html'},{name:'購物車',link:'shopcar.html'}],
+        socialLink:{
+            fb:'https://facebook.com/',
+            ig:'https://www.instagram.com/yuming7749/',
+            github:'https://github.com/devil1998871002'
+        },
+        email:'devil1998871002@gmail.com',
+
+        myshop:JSON.parse(localStorage.getItem("mycoffee")) || [],
+        total:list.reduce( (sun,i) => {return sun+(parseInt(i.pay,10)*parseInt(i.nums,10))},0),
+
+    },
+    methods:{
+        delcoffee(index){
+            // this.myshop=this.myshop.filter((item,i)=> {
+            //     return i!= index;
+            // });
+            this.myshop.splice(index,1)
+            localStorage.setItem('mycoffee',JSON.stringify(this.myshop));
+            this.total=this.myshop.reduce( (sun,i) => {return sun+(parseInt(i.pay,10)*parseInt(i.nums,10))},0)
+        }
+    },
+
+
+});
